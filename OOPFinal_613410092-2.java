7c836af806cb29fc88bc735e8f964356c5a46b5f
วงษกร พันธ์พิบูลย์ <58842048+crownvictoriaP71@users.noreply.github.com>
2020-04-01 15:54:16 +0700

    OOPFinal_613410092-2

---
+++ OOPFinal_613410092-2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.finalproject;
+
+/**
+ *
+ * @author TPEC
+ */
+public class ClassStudent {
+    String namestudent ;
+    String nicknamestudent ;
+    String phonenumberstudent ;
+    String locationworkingstudent ;
+    String provincestudent ;
+    String mentorstudent;
+}
